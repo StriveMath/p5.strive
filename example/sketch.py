dd0ea84f590bd832a6d6c41e87a1da291164946e
Nick McIntyre <nick@mcintyre.io>
2021-10-19 15:13:24 -0500

    Minimal fix for push() and pop()

--- example/sketch.py
+++ example/sketch.py
@@ -2,9 +2,24 @@ from p5 import *
 
 def setup():
   createCanvas(400, 400)
+  global p1
+  p1 = createMovableCircle(300, 300, 25)
 
 def draw():
   background('black')
-  translate(200, 200)
+
+  p1.draw()
+
   drawTickAxes()
-  circle(50, 100, 10)
+
+  push()
+  translate(200, 200)
+  scale(sin(frameCount))
+  rotate(-frameCount)
+  stroke('white')
+  line(0, 0, 100, 50)
+  pop()
+
+  translate(100, 200)
+  rotate(frameCount)
+  rect(0, 0, 80, 20)
